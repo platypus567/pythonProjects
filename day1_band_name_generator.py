a3ea35a32cc90765b68c743b65d4ad8433524146
whitehatns <63357002+platypus567@users.noreply.github.com>
2021-11-24 22:58:55 -0500

    Add files via upload

---
+++ day1_band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!\n")
+firstName = input("What city did you grow up in? \n")
+secondName = input("What is the name of your pet? \n ")
+print("Your band name could be " + firstName + " " + secondName)
