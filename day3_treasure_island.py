2f89af52c8ac947f3b1c996ec73d162ce0f2f648
whitehatns <63357002+platypus567@users.noreply.github.com>
2021-11-26 01:04:18 -0500

    Update day3_treasure_island.py

--- day3_treasure_island.py
+++ day3_treasure_island.py
@@ -23,17 +23,17 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 input_direction = input("You are at a crossroads. Do you go left or right? \n")
-if (input_direction == "right"):
+if (input_direction.lower() == "right"):
     print("Walking to the right, you stumble on a mine. Game Over.")
-elif (input_direction == "left"):
+elif (input_direction.lower() == "left"):
     input_swim = input("To the left, you arrive at a lake. Do you swim, or wait for a boat?\n")
-    if (input_swim == "swim"):
+    if (input_swim.lower() == "swim"):
         print("Attempting to swim the lake, you get fatigued and drown. Game Over.")
-    elif (input_swim == "wait"):
+    elif (input_swim.lower() == "wait"):
         input_doors = input("The boat takes you across, and you enter a building on the shore with three doors. One leads to the treasure, the others to certain demise.\n Do you enter the red, green, or blue door?\n")
-        if (input_doors == "red"):
+        if (input_doors.lower() == "red"):
             print("The room was full of men in hockey masks with machetes. You die. Game Over.")
-        elif (input_doors == "green"):
+        elif (input_doors.lower() == "green"):
             print("Nothing appears to be wrong at first, but the door to exit suddenly locks. After an hour, the gas kills you. Game Over.")
-        elif (input_doors == "blue"):
+        elif (input_doors.lower() == "blue"):
             print("The gold shines in front of you. You stuff as much as you can into your sack and leave, victorious. You Win.") 
