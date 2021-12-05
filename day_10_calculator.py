4c81cb01a59b9ef5b088386fc288c63feab84856
whitehatns <63357002+platypus567@users.noreply.github.com>
2021-12-05 01:43:14 -0500

    Add files via upload

---
+++ day_10_calculator.py
@@ -0,0 +1,74 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+operations = {"+": add, 
+"-": subtract,
+"*": multiply, 
+"/": divide}
+
+def calculator():
+  print(logo)
+  should_continue = True
+  num1 = float(input("What's the first number?\n"))
+  for key in operations:
+    print(key)
+  while should_continue == True:
+
+    operation = input("What operation are you using?\n")
+    num2 = float(input("What's the next number?\n"))
+    calculation_function = operations[operation]
+    answer = calculation_function(num1,num2)
+    print(f"{num1} {operation} {num2} = {answer}")
+    if input(f"Type y to continue calculating with {answer}, type n to start a new calculation\n") == "y":
+      num1 = answer
+      
+    else:
+      should_continue = False
+      print(chr(27) + "[2J")
+      calculator()
+calculator()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
