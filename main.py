a5e9fddbfd7f919761907e010a0577bf0625963b
whitehatns <63357002+platypus567@users.noreply.github.com>
2021-12-23 07:52:30 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,85 +1,29 @@
-water = 300
-milk = 300
-money = 0
-coffee = 300
-is_off = False
-def count_coins():
-    quarters = int(input("How many quarters?")) * 0.25
-    dimes = int(input("How many dimes?")) * 0.10
-    nickels = int(input("How many nickels?")) * 0.05
-    pennies = int(input("How many pennies?")) * 0.01
-    global money
-    money = float(quarters + dimes + nickels + pennies)
-    print("You have put in " + str(money) + " dollars.")
-    return money
-
-while is_off == False:
-    count_coins()
-    choice = input("Would you like an espresso/cappuccino/latte?")
-
-    if choice == "off":
-        is_off = True
-    elif choice == "report":
-        print("Water: " + str(water) + " mL")
-        print("Milk: " + str(milk) + " mL")
-        print("Coffee: " + str(coffee) + "g")
-        print("Money: $" + str(money))
-        break;
-
-    elif choice == "espresso":
-        if water < 100:
-            print("Not enough water")
-        elif coffee < 15:
-            print("Not enough coffee")
-            is_off = True
-        elif milk < 100:
-            print("Not enough milk")
-            is_off = True
-        else:
-            if money < 2:
-                print("Not enough money for espresso, try again.")
-
-            elif money >= 2:
-                print("Here is your espresso, enjoy!")
-                money -= 2
-                water -= 50
-
-    elif choice == "cappuccino":
-        if water < 100:
-            print("Not enough water")
-            is_off = True
-        elif coffee < 15:
-            print("Not enough coffee")
-            is_off = True
-        elif milk < 100:
-            print("Not enough milk")
-            is_off = True
-        else:
-            if money < 1:
-                print("Not enough money for cappuccino, try again.")
-
-            elif money >= 2:
-                print("Here is your cappuccino, enjoy!")
-                money -= 2
-                water -= 50
-
-    elif choice == "latte":
-        if water < 100:
-            print("Not enough water")
-        elif coffee < 15:
-            print("Not enough coffee")
-            is_off = True
-        elif milk < 100:
-            print("Not enough milk")
-            is_off = True
-
-        else:
-            if money < 1:
-                print("Not enough money for latte, try again.")
-
-            elif money >= 2:
-                print("Here is your latte, enjoy!")
-                water -= 50
-                money -= 2
+from turtle import Turtle, Screen
+screen = Screen()
+screen.setup(width=640,height=640)
+screen.bgcolor("black")
+screen.title("Snake Game")
+starting_positions = [(0,0),(-20,0),(-40,0)]
+segments = []
+for pos in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.penup()
+    new_segment.color('white')
+    new_segment.goto(pos)
+    segments.append(new_segment)
+game_is_on = True
+while game_is_on == True:
+    for segment in segments:
+        segment.forward(20)
 
 
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
